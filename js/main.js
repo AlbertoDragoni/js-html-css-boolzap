eb0d8f292881bdd30d6621225c62916994e23352
Alberto Dragoni <albertodragoni@yahoo.it>
2020-03-05 15:03:27 +0100

    prove invio messaggi

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
+$(document).ready(function(){
+    $('.microphone').click(function(){
+        var testoInput = $('#text').val();
+        $('#text').val('');
+        var messaggio = $('.template .messaggio').clone();
+        messaggio.children('.testo-messaggio').text(testoInput);
+        messaggio.children('.orario').text('05:05');
+        $('.screen-chat').append(messaggio);
+    });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
