11fa3901ed66f1b08aed06b5b16c6716f9b0b26c
Alberto Dragoni <albertodragoni@yahoo.it>
2020-03-05 15:14:05 +0100

    messaggi entrata ok

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
     $('.paper-plane').click(function(){
         var testoInput = $('#text').val();
         $('#text').val('');
-        var messaggio = $('.template .messaggio').clone();
-        messaggio.children('.testo-messaggio').text(testoInput);
-        messaggio.children('.orario').text('05:05');
+        var messaggio = $('.template .second-bubble').clone();
+        messaggio.children('.real-chat').text(testoInput);
+        messaggio.children('.time-chat').text('05:05');
         $('.screen-chat').append(messaggio);
     });
 
