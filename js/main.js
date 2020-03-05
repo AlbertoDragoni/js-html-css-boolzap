5d811a6966ac509d2ec2d2ff38a11658e8a26871
Alberto Dragoni <albertodragoni@yahoo.it>
2020-03-05 15:42:04 +0100

    received ok

--- js/main.js
+++ js/main.js
@@ -6,7 +6,32 @@ $(document).ready(function(){
         messaggio.children('.real-chat').text(testoInput);
         messaggio.children('.time-chat').text('05:05');
         $('.screen-chat').append(messaggio);
+        //messaggi risposta
+        var messaggioRisp = $('.template .third-bubble').clone();
+        messaggioRisp.children('.real-chat').text('ok');
+        messaggioRisp.children('.time-chat').text('06:06');
+        $('.screen-chat').append(messaggioRisp);
     });
+
+    function messageSent() {
+        $('.paper-plane').click(function(){
+            var testoInput = $('#text').val();
+            $('#text').val('');
+            var messaggio = $('.template .second-bubble').clone();
+            messaggio.children('.real-chat').text(testoInput);
+            messaggio.children('.time-chat').text('05:05');
+            $('.screen-chat').append(messaggio);
+        });
+    };
+
+    function messageReceived() {
+        $('.paper-plane').click(function(){
+            var messaggioRisp = $('.template .third-bubble').clone();
+            messaggioRisp.children('.real-chat').text('ok');
+            messaggioRisp.children('.time-chat').text('06:06');
+            $('.screen-chat').append(messaggioRisp);
+        });
+    };
 
 
 
