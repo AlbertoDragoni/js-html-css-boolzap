084514a510ab4b5ed921be32820e47e2c3b31ac9
Alberto Dragoni <albertodragoni@yahoo.it>
2020-03-06 17:17:37 +0100

    schede (milestone 3.1) ok

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
     $('#ricerca').keyup(function(event){
         var carattereFiltro = $(this).val().toLowerCase();
         $('.contatto').each(function(){
-            var nomeContatto =$(this).find('#nome').text().toLowerCase();
+            var nomeContatto = $(this).find('#nome').text().toLowerCase();
             if (nomeContatto.includes(carattereFiltro)) {
                 $(this).show();
             } else {
@@ -16,21 +16,33 @@ $(document).ready(function(){
         })
     });
 
+    $('.contatto').click(function(){
+        var cliccaNome = $(this).find('#nome').text();
+        var utente = $(this).data('codice-utente');
+
+        $('.screen-chat').each(function(){
+            if ($(this).data('codice-utente') == utente) {
+                $('.screen-chat').removeClass('active');
+                $(this).addClass('active');
+            }
+        })
+    });
 
     function messageSent() {
             var testoInput = $('#text').val();
             $('#text').val('');
             var messaggio = $('.template .second-bubble').clone();
+            console.log(messaggio)
             messaggio.children('.real-chat').text(testoInput);
             messaggio.children('.time-chat').text(oraGiusta);
-            $('.screen-chat').append(messaggio);
+            $('.screen-chat.active').append(messaggio);
     };
 
     function messageReceived() {
             var messaggioRisp = $('.template .third-bubble').clone();
             messaggioRisp.children('.real-chat').text('ok');
             messaggioRisp.children('.time-chat').text(oraGiusta);
-            $('.screen-chat').append(messaggioRisp);
+            $('.screen-chat.active').append(messaggioRisp);
     };
 
     function oraGiusta() {
